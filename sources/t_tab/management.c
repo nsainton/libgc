b3745e1514af819ec89ffaec09b6f3cbbaa7ddc3
nsainton <nsainton@student.42.fr>
2023-05-22 18:17:02 +0200

    Changed add and del functions to take elemsize into account

--- sources/t_tab/management.c
+++ sources/t_tab/management.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 16:24:39 by nsainton          #+#    #+#             */
-/*   Updated: 2023/05/22 16:32:31 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/05/22 18:16:36 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@ int	add_tab(t_tab *tab, void *elem)
 	if (tab->len == tab->size && realloc_tab(tab, 2 * tab->size))
 		return (COLLECTOR_ALLOCATION_ERROR);
 	ft_memcpy(tab->tab + tab->len, elem, tab->elemsize);
-	tab->len ++;
+	tab->len += tab->elemsize;
 	return (COLLECTOR_NO_ERROR);
 }
 
 void	delelem_tab(t_tab *tab, void (*del)(void *))
 {
+	if (! tab->len)
+		return ;
 	if (del)
 		del(tab->tab + tab->len);
-	tab->len --;
+	tab->len -= tab->elemsize;
 }
 
 void	del_tab(t_tab *tab)
